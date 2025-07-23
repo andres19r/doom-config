e8a1603d431eed88e795184b1448cb93519872eb
Andres Rivero <andres19rivero@gmail.com>
2025-07-23 11:23:15 -0400

    change theme and add fix jk for escape mode

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'catppuccin)
+(setq doom-theme 'doom-nord-aurora)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -172,3 +172,5 @@
 
 (global-visual-line-mode t)
 (setq treemacs-default-visit-action 'treemacs-visit-node-close-treemacs)
+
+(use-package! evil-escape :init (setq evil-escape-key-sequence "jk"))
