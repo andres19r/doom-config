947c1048104f39e9ab47ef51ed228942a716f822
Andres Rivero <andres19rivero@gmail.com>
2025-07-31 10:36:36 -0400

    change font to Monaspice

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14))
+(setq doom-font (font-spec :family "MonaspiceNe Nerd Font Mono" :size 13))
 ;; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
