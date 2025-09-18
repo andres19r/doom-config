f59e30b85803f11fddd31de8c4c44e2fc8c8cc28
Andres Rivero <andres19rivero@gmail.com>
2025-09-18 09:48:54 -0400

    fix(themes): change vterm colors for kanso-pearl

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "MonaspiceNe Nerd Font" :size 13))
+(setq doom-font (font-spec :family "SauceCodePro Nerd Font" :size 13))
 ;; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-nord-aurora)
+(setq doom-theme 'kanso-mist)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -141,7 +141,7 @@
                   (replace-match "" nil t)))
 	      (apply oldfn args)))
 
-;; (setq lsp-headerline-breadcrumb-enable t)
+(setq lsp-headerline-breadcrumb-enable t)
 ;; (lsp-treemacs-sync-mode 1)
 
 (require 'dap-firefox)
